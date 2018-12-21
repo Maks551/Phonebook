2a4d3e5816c8d89c1c2665d26ce3d506ce1e9129
Maxim Kurkudyk <Maksik551@gmail.com>
2018-12-21 19:30:13 +0200

    Fix

--- src/test/java/com/example/phonebook/TestUtil.java
+++ src/test/java/com/example/phonebook/TestUtil.java
@@ -36,8 +36,4 @@ public class TestUtil {
     public static RequestPostProcessor userHttpBasic(User user) {
         return SecurityMockMvcRequestPostProcessors.httpBasic(user.getLogin(), user.getPassword());
     }
-
-    public static RequestPostProcessor userAuth(User user) {
-        return SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(user.getLogin(), user.getPassword()));
-    }
 }
--- src/test/java/com/example/phonebook/web/RootControllerTest.java
+++ src/test/java/com/example/phonebook/web/RootControllerTest.java
@@ -2,22 +2,11 @@ package com.example.phonebook.web;
 
 import org.junit.jupiter.api.Test;
 
-import static com.example.phonebook.TestUtil.userAuth;
-import static com.example.phonebook.UserTestData.USER;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class RootControllerTest extends AbstractControllerTest {
-
-    @Test
-    void testPhonebooks() throws Exception {
-        mockMvc.perform(get("/phonebooks")
-                .with(userAuth(USER)))
-                .andDo(print())
-                .andExpect(view().name("phonebooks"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"));
-    }
 
     @Test
     void testLogin() throws Exception {
