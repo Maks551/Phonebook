45b28ee4e71df74c2e84e947331e504fc28575f1
Maksim Kurkudyk <35866347+Maks551@users.noreply.github.com>
2018-12-21 19:36:04 +0200

    Update phonebook.bat

--- phonebook.bat
+++ phonebook.bat
@@ -1,2 +1,2 @@
 call mvn -DskipTests=true clean package
-call java -Dspring.profiles.active="MySQL" -DDATABASE_URL="mysql://root:root@localhost:3306/phone_books" target/*.war
+call java -DDATABASE_URL="mysql://root:root@localhost:3306/phone_books" target/*.war
